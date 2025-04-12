a97ec65b4331ecdf498b48541e962db24ce5a54b
Rayiumir <swordart2050@gmail.com>
2025-04-13 02:43:09 +0330

    Added Parse the package name

--- src/Console/Commands/MakePackage.php
+++ src/Console/Commands/MakePackage.php
@@ -12,7 +12,13 @@ class MakePackage extends Command
      *
      * @var string
      */
-    protected $signature = 'make:package';
+    protected $signature = 'make:package
+                            {name : The name of the package (vendor/package-name)}
+                            {--namespace= : The namespace of the package}
+                            {--type=default : The type of package (default, admin-panel, api-service, theme)}
+                            {--with-tests : Include tests directory}
+                            {--with-config : Include config file}
+                            {--interactive : Run in interactive mode}';
 
     /**
      * The console command description.
@@ -27,10 +33,64 @@ class MakePackage extends Command
     }
 
     /**
+     * The base path of the package.
+     *
+     * @var string
+     */
+    protected string $packagePath;
+
+    /**
+     * The vendor name of the package.
+     *
+     * @var string
+     */
+    protected string $vendorName;
+
+    /**
+     * The package name (without vendor).
+     *
+     * @var string
+     */
+    protected string $packageName;
+
+    /**
+     * The package namespace.
+     *
+     * @var string
+     */
+    protected string $namespace;
+
+    /**
      * Execute the console command.
      */
     public function handle(): void
     {
-        //
+        $this->parsePackageName();
+    }
+
+    /**
+     * Parse the package name.
+     *
+     * @return void
+     */
+    protected function parsePackageName(): void
+    {
+        $name = $this->argument('name');
+        $parts = explode('/', $name);
+
+        if (count($parts) !== 2) {
+            $this->error('Invalid package name format. Use vendor/package-name');
+            exit(Command::FAILURE);
+        }
+
+        $this->vendorName = $parts[0];
+        $this->packageName = $parts[1];
+        $this->packagePath = base_path('packages/' . $this->vendorName . '/' . $this->packageName);
+
+        $namespace = $this->option('namespace');
+        if (empty($namespace)) {
+            $namespace = Str::studly($this->vendorName) . '\\' . Str::studly($this->packageName);
+        }
+        $this->namespace = $namespace;
     }
 }
