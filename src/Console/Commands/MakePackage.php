c173e169d7e74b6bca90e3d84f2c91c898dc8c80
Rayiumir <swordart2050@gmail.com>
2025-04-13 02:50:13 +0330

    Added Create composer.json file

--- src/Console/Commands/MakePackage.php
+++ src/Console/Commands/MakePackage.php
@@ -72,6 +72,7 @@ class MakePackage extends Command
         }
 
         $this->createPackageDirectory();
+        $this->createPackageStructure();
     }
 
     /**
@@ -145,5 +146,96 @@ class MakePackage extends Command
         }
 
         File::makeDirectory($this->packagePath, 0755, true);
+    }
+
+    /**
+     * Create package directory structure.
+     *
+     * @return void
+     */
+    protected function createPackageStructure(): void
+    {
+        $folders = [
+            'src',
+            'resources/views',
+            'resources/lang',
+            'database/migrations',
+            'routes',
+        ];
+
+        // Create specific structure based on package type
+        $type = $this->option('type');
+        if ($type === 'admin-panel') {
+            $folders[] = 'resources/assets/js';
+            $folders[] = 'resources/assets/css';
+        } elseif ($type === 'api-service') {
+            $folders[] = 'src/Http/Controllers/Api';
+            $folders[] = 'src/Http/Resources';
+        } elseif ($type === 'theme') {
+            $folders[] = 'resources/assets/js';
+            $folders[] = 'resources/assets/css';
+            $folders[] = 'resources/assets/images';
+        }
+
+        foreach ($folders as $folder) {
+            File::makeDirectory($this->packagePath . '/' . $folder, 0755, true);
+        }
+
+        // Create empty .gitkeep files to ensure directories are tracked in git
+        foreach ($folders as $folder) {
+            File::put($this->packagePath . '/' . $folder . '/.gitkeep', '');
+        }
+    }
+
+    /**
+     * Create composer.json file.
+     *
+     * @return void
+     */
+    protected function createComposerJson(): void
+    {
+        $namespaceForJson = str_replace('\\', '\\\\', $this->namespace);
+        $composerJson = <<<JSON
+{
+    "name": "{$this->vendorName}/{$this->packageName}",
+    "description": "A Laravel package",
+    "type": "library",
+    "license": "MIT",
+    "authors": [
+        {
+            "name": "Your Name",
+            "email": "email@example.com"
+        }
+    ],
+    "require": {
+        "php": "^8.0",
+        "illuminate/support": "^9.0|^10.0|^11.0|^12.0"
+    },
+    "require-dev": {
+        "phpunit/phpunit": "^9.0|^10.0"
+    },
+    "autoload": {
+        "psr-4": {
+            "{$namespaceForJson}\\": "src/"
+        }
+    },
+    "autoload-dev": {
+        "psr-4": {
+            "{$namespaceForJson}\\Tests\\": "tests/"
+        }
+    },
+    "extra": {
+        "laravel": {
+            "providers": [
+                "{$this->namespace}\\{$this->getServiceProviderName()}"
+            ]
+        }
+    },
+    "minimum-stability": "dev",
+    "prefer-stable": true
+}
+JSON;
+
+        File::put($this->packagePath . '/composer.json', $composerJson);
     }
 }
